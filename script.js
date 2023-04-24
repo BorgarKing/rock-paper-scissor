9ce5f482f722f35db96873c4918e1de7b622b7aa
BorgAr <omar.adham.b@gmail.com>
2023-04-24 20:50:02 +0200

    add game function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ userInput = prompt ("rock paper scissors ?");
 
 inputCheck (userInput);
 
-//This function makes sure that the user input is either rock paper or scissors 
+//inputCheck is a function that makes sure the user input is either rock paper or scissors 
 
 function inputCheck (userInput){
 
@@ -13,13 +13,42 @@ function inputCheck (userInput){
         alert ("it's rock paper scissors man how hard can it be come on !");
         window.userInput= prompt("Try again");
         return (inputCheck(window.userInput));
+        
     }
 }
 
 console.log (userInput);
 
-//PC choice is a random selection from the choices array
+//getPcChoice is a function that returns a random selection from the choices array
 
 const choices = ['rock', 'paper', 'scissors'];    
-const pcChoice = Math.floor(Math.random() * choices.length);
-console.log(choices[pcChoice]);
+
+function getPcChoice (x){
+
+const pcChoice = Math.floor(Math.random() * x.length);
+console.log(x[pcChoice]);
+return (x[pcChoice]);
+
+}
+
+let pc= getPcChoice(choices);
+
+function game (pc,userInput){
+if (userInput==pc){
+    return ("oi it's a draw")
+}  
+ if (userInput== "rock"){
+    if(pc=="paper"){return("Paper beats rock dumb shit")}
+    else if (pc == "scissors") {return("congrats shit head")}
+ }
+ if (userInput== "paper"){
+    if(pc=="scissors"){return("scissors beat paper dumb shit")}
+    else if (pc == "rock") {return("congrats shit head")}
+ }
+ if (userInput== "scissors"){
+    if(pc=="rock"){return("Rock beats scissors dumb shit")}
+    else if (pc == "paper") {return("congrats shit head")}
+ }
+  
+}
+game (pc,userInput);
